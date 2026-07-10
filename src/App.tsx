04557ce9af44b4d988c6c30d8d5e62cf14511656
mssxmt <orela.rec@gmail.com>
2026-07-11 08:10:49 +0900

    feat: render procedural machine with R3F

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Canvas } from '@react-three/fiber'
+import { MachineObject } from './machine/MachineObject'
+
 export default function App() {
-  return <div style={{ color: '#fff' }}>VJ</div>
+  return (
+    <Canvas camera={{ position: [0, 2, 8], fov: 50 }} gl={{ antialias: true }}>
+      <color attach="background" args={['#000000']} />
+      <ambientLight intensity={0.2} />
+      <directionalLight position={[5, 10, 5]} intensity={2} />
+      <pointLight position={[-5, -5, -5]} intensity={0.5} />
+      <MachineObject />
+    </Canvas>
+  )
 }
